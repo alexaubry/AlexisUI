c6091558f82edda3e7aa62ac69f44e7dc91f7053
Alexis Aubry <me@alexaubry.fr>
2020-04-04 18:00:35 -0400

    Add StandaloneScrollView

---
+++ Sources/AlexisUI/Views/StandaloneScrollView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+/**
+ * A scroll view that is meant to be used outside of a navigation stack and that provides its own status bar cover.
+ */
+
+public struct StandaloneScrollView<Content: View>: View {
+    let content: Content
+
+    public init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+
+    public var body: some View {
+        ZStack {
+            ScrollView {
+                content
+            }
+
+            GeometryReader { geometry in
+                VStack {
+                    BlurView(light: .light, dark: .dark)
+                        .frame(width: geometry.size.width,
+                               height: geometry.safeAreaInsets.top,
+                               alignment: .top)
+
+                    Spacer()
+                }
+            }.edgesIgnoringSafeArea(.all)
+        }.background(Color(.secondarySystemBackground).edgesIgnoringSafeArea(.all))
+    }
+}
