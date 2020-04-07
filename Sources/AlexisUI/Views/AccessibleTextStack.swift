4705227d68af860fd5d87095c55278d5bd3dbfec
Alexis Aubry <me@alexaubry.fr>
2020-04-07 12:18:14 -0400

    Add accessible stack spacing

--- Sources/AlexisUI/Views/AccessibleTextStack.swift
+++ Sources/AlexisUI/Views/AccessibleTextStack.swift
@@ -2,20 +2,22 @@ import SwiftUI
 
 public struct AccessibleTextStack<Content: View>: View {
     let content: Content
+    let horizontalSpacing: CGFloat?
+    let verticalSpacing: CGFloat?
     @Environment(\.sizeCategory) var sizeCategory
 
-    public init(@ViewBuilder content: () -> Content) {
+    public init(horizontalSpacing: CGFloat? = nil, verticalSpacing: CGFloat? = nil, @ViewBuilder content: () -> Content) {
         self.content = content()
     }
 
     public var body: some View {
         Group {
             if sizeCategory.alx_isAccessibilityCategory {
-                VStack(alignment: .leading) {
+                VStack(alignment: .leading, spacing: verticalSpacing) {
                     content
                 }
             } else {
-                HStack(alignment: .firstTextBaseline) {
+                HStack(alignment: .firstTextBaseline, spacing: horizontalSpacing) {
                     content
                 }
             }
